c25150a5e1286851fd1f3998a282a3b147792689
Barry Williams <bdog84w@gmail.com>
2017-11-29 23:44:01 -0800

    styling edits leveraging bootswatch

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,7 +11,7 @@
 // loss counter increments if total > random number
 // game resets random number and total when game over
 
-window.onload = function() {
+// window.onload = function() {
 
 
 
@@ -29,42 +29,7 @@ var win = 0;
 // 	var total = 0;
 
 
-	// dynamically create image elements for crystals as buttons
 
-	// crystal 1
-	var imageCrystal1 = $("<img>");
-	// First each crystal will be given the class ".crystal-image".
-	// This will allow the CSS to take effect. 
-	imageCrystal1.addClass("crystal1 crystal-image");
-	// Each imageCrystal will be given a src link to the crystal image
-	imageCrystal1.attr("src", "http://kristalcanada.com/EN/wp-content/uploads/2013/10/RUBY-CLUSTER-MAP-672-small.jpg");
-	// Each imageCrystal will be given a data attribute called data-crystalValue.
-	// This data attribute will be set equal to the array value.
-	// imageCrystal1.text(crystNum1);
-	// Lastly, each crystal image (with all it classes and attributes) will get added to the page.
-	$(".crystals").append(imageCrystal1);
-
-	// repeat above for 3 more crystals
-	// crystal 2
-	var imageCrystal2 = $("<img>");
-	imageCrystal2.addClass("crystal2 crystal-image");
-	imageCrystal2.attr("src", "https://i.pinimg.com/originals/4c/ba/b3/4cbab33f7c850a4a28d7055e1eb23e21.jpg");
-	// imageCrystal2.attr("data-crystalvalue", crystNum2);
-	$(".crystals").append(imageCrystal2);
-
-	// crystal 3
-	var imageCrystal3 = $("<img>");
-	imageCrystal3.addClass("crystal3 crystal-image");
-	imageCrystal3.attr("src", "https://i.pinimg.com/originals/0a/ba/87/0aba87d2cb9891c9e10d4c4a4045d62a.jpg");
-	// imageCrystal3.attr("data-crystalvalue", crystNum3);
-	$(".crystals").append(imageCrystal3);
-
-	// crystal 4
-	var imageCrystal4 = $("<img>");
-	imageCrystal4.addClass("crystal4 crystal-image");
-	imageCrystal4.attr("src", "https://static.pexels.com/photos/159185/amethyst-violet-crystal-cave-druze-159185.jpeg");
-	// imageCrystal4.attr("data-crystalvalue", crystNum4);
-	$(".crystals").append(imageCrystal4);
 
 /////////////////////////
 
@@ -73,6 +38,8 @@ var win = 0;
 	console.log("Random Number = " + randomNum);
 	// jQuery syntax for grabbing element for random number and displaying on DOM
 	$("#start-number").html("Random Number: " + randomNum);
+
+
 
 	// generate and assign random number between 1-12 to each of the crystals
 	// first generate random number between 1-12 for a single crystal
@@ -111,6 +78,43 @@ var win = 0;
 // }; // close bracket for startGame function
 
 ///////////////////
+
+	// dynamically create image elements for crystals as buttons
+
+	// crystal 1
+	var imageCrystal1 = $("<img>");
+	// First each crystal will be given the class ".crystal-image".
+	// This will allow the CSS to take effect. 
+	imageCrystal1.addClass("crystal1 crystal-image");
+	// Each imageCrystal will be given a src link to the crystal image
+	imageCrystal1.attr("src", "http://kristalcanada.com/EN/wp-content/uploads/2013/10/RUBY-CLUSTER-MAP-672-small.jpg");
+	// Each imageCrystal will be given a data attribute called data-crystalValue.
+	// This data attribute will be set equal to the array value.
+	// imageCrystal1.text(crystNum1);
+	// Lastly, each crystal image (with all it classes and attributes) will get added to the page.
+	$(".crystals").append(imageCrystal1);
+
+	// repeat above for 3 more crystals
+	// crystal 2
+	var imageCrystal2 = $("<img>");
+	imageCrystal2.addClass("crystal2 crystal-image");
+	imageCrystal2.attr("src", "https://i.pinimg.com/originals/4c/ba/b3/4cbab33f7c850a4a28d7055e1eb23e21.jpg");
+	// imageCrystal2.attr("data-crystalvalue", crystNum2);
+	$(".crystals").append(imageCrystal2);
+
+	// crystal 3
+	var imageCrystal3 = $("<img>");
+	imageCrystal3.addClass("crystal3 crystal-image");
+	imageCrystal3.attr("src", "https://i.pinimg.com/originals/0a/ba/87/0aba87d2cb9891c9e10d4c4a4045d62a.jpg");
+	// imageCrystal3.attr("data-crystalvalue", crystNum3);
+	$(".crystals").append(imageCrystal3);
+
+	// crystal 4
+	var imageCrystal4 = $("<img>");
+	imageCrystal4.addClass("crystal4 crystal-image");
+	imageCrystal4.attr("src", "https://static.pexels.com/photos/159185/amethyst-violet-crystal-cave-druze-159185.jpeg");
+	// imageCrystal4.attr("data-crystalvalue", crystNum4);
+	$(".crystals").append(imageCrystal4);
 
 // add value of each button to total when a crystal button is clicked
 	$(".crystal1").on("click", function() {
@@ -189,18 +193,12 @@ function decision() {
 }; //close bracket for decision function
 
 
-
 // game resets random number and total when game over
 
-// function gameOver() {
-// 	randomNum = 0;
-// 	total = 0;
-// 	// startGame();
-// } // close bracket for gameOver function
 
 // startGame();
 
-} // close bracket for window.onload function
+// } // close bracket for window.onload function
 
 
 
