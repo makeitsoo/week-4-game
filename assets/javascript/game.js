29895caa3cc6d220923295a7e0a2536ef7943d83
Barry Williams <bdog84w@gmail.com>
2017-11-29 20:12:31 -0800

    modified code to append random button value to the crystal buttons only after initial click so values are hidden from user until clicked

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -36,19 +36,19 @@ var win = 0;
 
 	var crystNum1 = Math.floor(Math.random() * 12 ) + 1;
 	console.log("crystal1 = " + crystNum1);
-	$("#crystal1").append(crystNum1);
+	// $("#crystal1").append(crystNum1);
 
 	var crystNum2 = Math.floor(Math.random() * 12 ) + 1;
 	console.log("crystal2 = " + crystNum2);
-	$("#crystal2").append(crystNum2);
+	// $("#crystal2").append(crystNum2);
 
 	var crystNum3 = Math.floor(Math.random() * 12 ) + 1;
 	console.log("crystal3 = " + crystNum3);
-	$("#crystal3").append(crystNum3);
+	// $("#crystal3").append(crystNum3);
 
 	var crystNum4 = Math.floor(Math.random() * 12 ) + 1;
 	console.log("crystal4 = " + crystNum4);
-	$("#crystal4").append(crystNum4);
+	// $("#crystal4").append(crystNum4);
 
 
 // //take the above concept and apply to all 4 crystal buttons with loop
@@ -70,6 +70,7 @@ var win = 0;
 
 // add value of each button to total when a crystal button is clicked
 	$("#crystal1").on("click", function() {
+		$("#crystal1").append(crystNum1);
 		// console.log("total before: " + total);
 		total = total + crystNum1;
 		console.log("total after: " + total);
@@ -79,6 +80,7 @@ var win = 0;
 	});
 
 	$("#crystal2").on("click", function() {
+		$("#crystal2").append(crystNum2);
 		// console.log("total before: " + total);
 		total = total + crystNum2;
 		console.log("total after: " + total);
@@ -88,6 +90,7 @@ var win = 0;
 	});
 
 	$("#crystal3").on("click", function() {
+		$("#crystal3").append(crystNum3);
 		// console.log("total before: " + total);
 		total = total + crystNum3;
 		console.log("total after: " + total);
@@ -97,6 +100,7 @@ var win = 0;
 	});
 
 	$("#crystal4").on("click", function() {
+		$("#crystal4").append(crystNum4);
 		// console.log("total before: " + total);
 		total = total + crystNum4;
 		console.log("total after: " + total);
